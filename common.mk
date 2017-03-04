c92a9af1769836c722378f2e30cc68a9757f1dbe
offain <fabian.leutenegger@bluewin.ch>
2017-03-04 23:32:58 +0100

    Add missing RCS packages
    These are present in caf based device trees, but not on others.
    
    Change-Id: I031f2adfcabf121e7734e17a920c1e2fc489d645

--- common.mk
+++ common.mk
@@ -131,6 +131,9 @@ PRODUCT_PACKAGES += libbthost_if
 
 # RCS
 PRODUCT_PACKAGES += \
+    rcscommon \
+    rcscommon.xml \
+    rcsservice \
     rcs_service_aidl \
     rcs_service_aidl.xml \
     rcs_service_aidl_static \
